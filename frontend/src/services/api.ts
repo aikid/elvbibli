4ca74f361e08612d9114e76b6d6417f0eb31f5f2
Leandro Brito <klonoa51@gmail.com>
2026-02-18 19:11:36 -0300

    atualizacao de varialvel de ambiente

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,12 @@
 import axios from "axios";
 
+const baseURL =
+  import.meta.env.MODE === "development"
+    ? import.meta.env.VITE_API_URL
+    : "";
+
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: baseURL,
   headers: {
     "Content-Type": "application/json",
   },
